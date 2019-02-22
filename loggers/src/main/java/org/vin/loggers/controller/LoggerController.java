5a065901a366fb1be4a55c1816f28f51504e12d5
vincent <fanyulu4w@gmail.com>
2019-02-22 21:43:40 +0800

    second

---
+++ loggers/src/main/java/org/vin/loggers/controller/LoggerController.java
@@ -0,0 +1,29 @@
+package org.vin.loggers.controller;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class LoggerController {
+
+	private static final  org.slf4j.Logger logger = LoggerFactory.getLogger(LoggerController.class) ;
+
+	@PostMapping("log")
+	public  String  log(@RequestParam("log") String logJson){
+		System.out.println(logJson);
+		JSONObject jsonObject = JSON.parseObject(logJson);
+		jsonObject.put("ts",System.currentTimeMillis());
+
+		logger.info(jsonObject.toJSONString());
+
+		return   "success";
+	}
+
+}
