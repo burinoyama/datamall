1c9db112dd75a1c42ec5dcc375b852ccf76f0229
vincent <fanyulu4w@gmail.com>
2019-02-26 19:49:30 +0800

    2

--- loggers/src/main/java/org/vin/loggers/controller/LoggerController.java
+++ loggers/src/main/java/org/vin/loggers/controller/LoggerController.java
@@ -14,7 +14,7 @@ public class LoggerController {
 
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(LoggerController.class);
 
-	@PostMapping("log" )
+	@PostMapping("log")
 	public String log(@RequestParam("log") String logJson) {
 		System.out.println(logJson);
 		JSONObject jsonObject = JSON.parseObject(logJson);
